1b1b71ba31331901cfa8547a241aac7385d7d5cb
Vladimir Gorej <gorej@codescale.net>
2011-07-31 16:41:26 +0200

    Initial implementation of classes and workflow

--- pybrowscap/__init__.py
+++ pybrowscap/__init__.py
@@ -1,17 +1,104 @@
 __version__ = '1.0b1'
 
-import csv
+class Browser(object):
 
-with open('../browscap.csv', 'rb') as csvfile:
-    dialect = csv.Sniffer().sniff(csvfile.read(4096))
-    csvfile.seek(0)
-    csvfile.readline()
-    csvfile.readline()
-    reader = csv.DictReader(csvfile, dialect=dialect)
-    defaults = {}
-    for line in reader:
-        if reader.line_num == 2:
-            defaults = line
-            continue
+    def __init__(self, defaults, user_agent):
+        self.user_agent = {}
+        for feature, value in user_agent.iteritems():
+            if value == 'default':
+                value = defaults[feature]
+            if value == 'true':
+                value = True
+            if value == 'false':
+                value = False
+            if feature.lower() == 'majorversion' or feature.lower() == 'minorversion':
+                try:
+                    value = int(value)
+                except Exception:
+                    value = 0
+            if feature.lower() == 'cssversion':
+                value = float(value)
+            self.user_agent[feature.lower()] = value
 
-        print line
+    def name(self):
+        return self.user_agent.get('browser')
+
+    def category(self):
+        return self.user_agent.get('parent')
+
+    def version(self):
+        return self.user_agent.get('version')
+
+    def version_major(self):
+        return int(self.user_agent.get('majorversion', 0))
+
+    def version_minor(self):
+        return int(self.user_agent.get('minorversion', 0))
+
+    def css_version(self):
+        return int(self.user_agent.get('cssversion', 0))
+
+    def supports(self, feature):
+        return self.user_agent.get(feature, False)
+
+    def supports_tables(self):
+        return self.supports('tables')
+
+    def supports_frames(self):
+        return self.supports('frames')
+
+    def supports_iframes(self):
+        return self.supports('iframes')
+
+    def supports_java(self):
+        return self.supports('javaapplets')
+
+    def supports_javascript(self):
+        return self.supports('javascript')
+
+    def supports_vbscript(self):
+        return self.supports('vbscript')
+
+    def supports_activex(self):
+        return self.supports('activexcontrols')
+
+    def supports_cookies(self):
+        return self.supports('cookies')
+
+    def supports_css(self):
+        return self.css_version() > 0
+
+    def is_crawler(self):
+        return self.supports('crawler')
+
+    def is_mobile(self):
+        return self.supports('mobiledevice')
+
+    def is_syndication_reader(self):
+        return self.supports('syndicationreader')
+
+    def is_banned(self):
+        return self.supports('banned')
+
+    def is_alpha(self):
+        return self.supports('alpha')
+
+    def is_beta(self):
+        return self.supports('beta')
+
+    def features(self):
+        features = []
+        for feature in ['tables', 'frames', 'iframes', 'javascript', 'vbscript', 'cookies']:
+            if self.supports(feature) is True:
+                features.append(feature)
+        if self.supports_activex():
+            features.append('activex')
+        if self.supports_java():
+            features.append('java')
+        if self.css_version() > 0:
+            features.append('css1')
+        if self.css_version() > 1:
+            features.append('css2')
+        if self.css_version() > 2:
+            features.append('css3')
+        return features
--- pybrowscap/loader/__init__.py
+++ pybrowscap/loader/__init__.py
@@ -1,5 +1,8 @@
+from pybrowscap import Browser
 import logging
 import urllib2
+import csv
+import re
 
 log = logging.getLogger(__name__)
 
@@ -14,10 +17,10 @@ class Downloader(object):
 
     def get(self, save_to_filepath=None):
         try:
-            log.debug('Downloading latest version of browscap from %s' % self.url)
+            log.info('Downloading latest version of browscap from %s' % self.url)
             opener = urllib2.build_opener()
             if self.proxy is not None:
-                log.debug('Setting up proxy server %s' % self.proxy)
+                log.info('Setting up proxy server %s' % self.proxy)
                 opener.add_handler(urllib2.ProxyHandler({'http': self.proxy}))
                 for handler in self.additional_handlers:
                     opener.add_handler(handler)
@@ -27,18 +30,65 @@ class Downloader(object):
             response.close()
         except Exception:
             log.exception('Error while downloading latest version of browscap')
+            raise
 
         if save_to_filepath is not None:
             try:
-                log.debug('Saving latest version of browscap file to %s' % save_to_filepath)
-                file = open(save_to_filepath, 'wb')
-                file.write(contents)
-                file.close()
+                log.info('Saving latest version of browscap file to %s' % save_to_filepath)
+                with open(save_to_filepath, 'wb') as file:
+                    file.write(contents)
             except Exception:
                 log.exception('Error while saving latest version of browscap file')
+                raise
         else:
             return contents
 
 
-class Loader(object):
-    pass
+class Browscap(object):
+
+    cache = {}
+
+    def __init__(self, defaults, data_dict):
+        self.defaults = defaults
+        self.data     = data_dict
+
+    def search(self, user_agent_string):
+        user_agent_string = '[%s]' % user_agent_string
+
+        ua_regex_pattern = ''
+        for ua_regex, line in self.data.iteritems():
+            if ua_regex.match(user_agent_string) and len(ua_regex.pattern) > len(ua_regex_pattern):
+                ua_regex_pattern = ua_regex.pattern
+        if ua_regex_pattern == '':
+            return None
+        else:
+            ua_regex = re.compile(ua_regex)
+            return Browser(self.defaults, self.data[ua_regex])
+
+def load_file(browscap_file_path):
+    with open(browscap_file_path, 'rb') as csvfile:
+        log.info('Reading browscap source file')
+        dialect = csv.Sniffer().sniff(csvfile.read(4096))
+        csvfile.seek(0)
+        log.info('Removing fileinfo section')
+        csvfile.readline()
+        csvfile.readline()
+        reader = csv.DictReader(csvfile, dialect=dialect)
+        defaults = {}
+        browscap_data = {}
+        for line in reader:
+            if reader.line_num == 2:
+                defaults = line
+                continue
+            ua_regex = line['UserAgent']
+            for unsafe_char in '^$()[].-':
+                ua_regex = ua_regex.replace(unsafe_char, '\%s' % unsafe_char)
+            ua_regex = ua_regex.replace('?', '.').replace('*', '.*?')
+            ua_regex = '^%s$' % ua_regex
+            browscap_data.update({re.compile(ua_regex): line})
+    return Browscap(defaults, browscap_data)
+
+
+browscap = load_file('/var/projects/python/pybrowscap/browscap.csv')
+browser = browscap.search('[Mozilla/5.0 (X11; U; Linux i686; de; rv:1.8.0.5) Gecko/20060731 Ubuntu/dapper-security Firefox/1.5.0.5]')
+print browser.name()
