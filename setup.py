3dcfd0033882f06bc5af1bc96c4dc2b9bd7ee7fd
Vladimir Gorej <gorej@codescale.net>
2011-08-01 16:09:03 +0200

    Added setup file for pypi package system

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+import pybrowscap
+import os
+
+# Utility function to read the README file.
+# Used for the long_description. It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='pybrowscap',
+    version=pybrowscap.__version__,
+    description='detects browsers capabilities and features like css, java, javascript etc.',
+    long_description=read('README'),
+    author='Vladimír Gorej',
+    author_email='gorej@codescale.net',
+    url='http://www.codescale.net/en/community#pybrowscap',
+    license='BSD',
+    keywords = "browser browscap detection user agent",
+    packages=['pybrowscap']
+)
+
