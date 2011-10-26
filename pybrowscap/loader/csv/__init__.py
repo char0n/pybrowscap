41e757ea208770582afd7661aa275094248a39c2
kinpoo <kinpoo@gmail.com>
2011-10-26 23:51:46 +0800

    use re.escape to escape all unsafe chars (include "+", etc)

--- pybrowscap/loader/csv/__init__.py
+++ pybrowscap/loader/csv/__init__.py
@@ -81,11 +81,8 @@ def load_file(browscap_file_path):
                     defaults = line
                     continue
                 line = replace_defaults(line, defaults)
-                ua_regex = line['useragent'][1:-1]
-                for unsafe_char in '^$()[].-':
-                    ua_regex = ua_regex.replace(unsafe_char, '\%s' % unsafe_char)
-                ua_regex = ua_regex.replace('?', '.').replace('*', '.*?')
-                ua_regex = '^%s$' % ua_regex
+                ua_regex = '^%s$' % re.escape(line['useragent'][1:-1])
+                ua_regex = ua_regex.replace('\\?', '.').replace('\\*', '.*?')
                 browscap_data.update({ua_regex: line})
                 regex_cache.append(re.compile(ua_regex))
         return Browscap(
