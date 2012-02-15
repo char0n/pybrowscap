46071604aecf98ff9045b5739c1c4e45487a7e57
Vladimir Gorej <gorej@codescale.net>
2012-02-15 21:41:44 +0100

    Updated setup file

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 import os
 from setuptools import setup
+
 import pybrowscap
 
-# Utility function to read the README file.
-# Used for the long_description. It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
 def read(fname):
+    """Utility function to read the README file.
+
+    Used for the long_description. It's nice, because now 1) we have a top level
+    README file and 2) it's easier to type in the README file than to put a raw
+    string in below ...
+
+    """
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
@@ -19,7 +23,24 @@ setup(
     author=u'Vladimír Gorej',
     author_email='gorej@codescale.net',
     url='http://www.codescale.net/en/community#pybrowscap',
+    download_url='http://github.com/char0n/pybrowscap/tarball/master',
     license='BSD',
     keywords = 'browser browscap detection user agent',
-    packages=['pybrowscap', 'pybrowscap.loader', 'pybrowscap.loader.csv']
+    packages=['pybrowscap', 'pybrowscap.loader', 'pybrowscap.loader.csv'],
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Browsers'
+    ],
+    test_suite='pybrowscap.test',
 )
